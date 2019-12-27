e2e5c1d073bd5be113c256ccec6ed4e41bc52807
Gergely Pap <pgege42@gmail.com>
2019-12-27 10:18:44 +0100

    Convert List to stateless function component

--- 02-todomvc/src/components/List.jsx
+++ 02-todomvc/src/components/List.jsx
@@ -1,26 +1,25 @@
 import React from "react";
 import "./List.css";
 
-export default class List extends React.Component {
-  render() {
-    return (
-      <div className="list">
-        {!this.props.items.length ? (
-          <p>No tasks</p>
-        ) : (
-          this.props.items.map(item => (
-            <span
-              className={["list-item", item.done ? "list-item--done" : ""].join(
-                " "
-              )}
-              key={item.id}
-              onClick={() => this.props.onTodoClick(item)}
-            >
-              {item.title}
-            </span>
-          ))
-        )}
-      </div>
-    );
+const List = ({ items, onTodoClick }) => {
+  if (!items.length) {
+    return <p>No tasks</p>;
   }
-}
+  return (
+    <div className="list">
+      {items.map(item => (
+        <span
+          className={["list-item", item.done ? "list-item--done" : ""].join(
+            " "
+          )}
+          key={item.id}
+          onClick={() => onTodoClick(item)}
+        >
+          {item.title}
+        </span>
+      ))}
+    </div>
+  );
+};
+
+export default List;
