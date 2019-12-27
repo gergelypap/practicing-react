eaffbe52ef756d1c24850e98db8c30e6b6f42918
Gergely Pap <pgege42@gmail.com>
2019-12-27 23:45:12 +0100

    Extract ListItem into a separate component

--- 03-todomvc-redux/src/components/List.jsx
+++ 03-todomvc-redux/src/components/List.jsx
@@ -1,25 +1,17 @@
 import React from "react";
 import "./List.css";
 import PropTypes from "prop-types";
+import ListItem from "./ListItem";
 
-const List = ({ todos, onClick }) => {
-  if (!todos.length) {
-    return <p className="list list--empty">No tasks</p>;
-  }
-  return (
-    <div className="list">
-      {todos.map(item => (
-        <span
-          className={"list-item" + (item.done ? " list-item--done" : "")}
-          key={item.id}
-          onClick={() => onClick(item.id)}
-        >
-          {item.text}
-        </span>
-      ))}
-    </div>
-  );
-};
+const List = ({ todos, onClick }) => (
+  <div className={"list" + (!todos.length ? " list--empty" : "")}>
+    {todos.length
+      ? todos.map(item => (
+          <ListItem key={item.id} item={item} onClick={onClick} />
+        ))
+      : "No tasks"}
+  </div>
+);
 
 List.propTypes = {
   todos: PropTypes.arrayOf(
