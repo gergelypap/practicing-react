19c1b7f6d1d4d87d840f5708e06ca306ec10e7d5
Gergely Pap <pgege42@gmail.com>
2019-12-28 19:19:46 +0100

    Simplify dispatch actions in FilterContainer and Filter

--- 03-todomvc-redux/src/components/Filter.jsx
+++ 03-todomvc-redux/src/components/Filter.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import "./Filter.css";
 import PropTypes from "prop-types";
+import { FILTER } from "../constants";
 
-const Filter = ({ count, active, onClick, children }) => (
-  <button className="filter" onClick={onClick} disabled={active}>
-    {children} ({count})
-  </button>
-);
+const Filter = ({ count, active, setFilter, children, filter }) => {
+  return (
+    <button
+      className="filter"
+      onClick={() => setFilter(filter)}
+      disabled={active}
+    >
+      {children} ({count})
+    </button>
+  );
+};
 
 Filter.propTypes = {
   count: PropTypes.number.isRequired,
   active: PropTypes.bool.isRequired,
-  onClick: PropTypes.func.isRequired,
-  children: PropTypes.string.isRequired
+  setFilter: PropTypes.func.isRequired,
+  children: PropTypes.string.isRequired,
+  filter: PropTypes.oneOf(Object.keys(FILTER))
 };
 
 export default Filter;
--- 03-todomvc-redux/src/containers/FilterContainer.js
+++ 03-todomvc-redux/src/containers/FilterContainer.js
@@ -3,7 +3,6 @@ import { connect } from "react-redux";
 import "../components/Filters.css";
 import Filter from "../components/Filter";
 import { FILTER } from "../constants";
-import PropTypes from "prop-types";
 
 const getCount = (todos, filter) => {
   if (filter === FILTER.SHOW_ALL) {
@@ -19,16 +18,8 @@ const state = (state, ownProps) => ({
   count: getCount(state.todos, ownProps.filter)
 });
 
-const actions = (dispatch, ownProps) => ({
-  onClick: () => {
-    dispatch(setFilter(ownProps.filter));
-  }
-});
-
-const FilterContainer = connect(state, actions)(Filter);
-
-FilterContainer.propTypes = {
-  filter: PropTypes.oneOf(Object.keys(FILTER))
+const actions = {
+  setFilter
 };
 
-export default FilterContainer;
+export default connect(state, actions)(Filter);
